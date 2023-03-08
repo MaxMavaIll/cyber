6f2e07e098b0fc9235bb797d1d5ed9366440a3ea
illva <102728347+MaxMavaIll@users.noreply.github.com>
2023-03-08 14:35:22 +0200

    Update config.py

--- api/config.py
+++ api/config.py
@@ -5,5 +5,5 @@ env.read_env()
 API_TOKEN = env.str("MINT_SCAN_API_TOKEN")
 
 nodes = {
-    "juno": ["/root/go/bin/junod","http://65.21.132.27:28057"],
+    "<network>": ["/root/go/bin/<bin>","<rpc>"],
 }
